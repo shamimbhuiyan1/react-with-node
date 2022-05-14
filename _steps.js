7fa79c81290619c3258bdce695ebf81e68fe4439
shamimbhuiyan1 <shamimbhuiyan1508@gmail.com>
2022-05-15 02:04:31 +0600

    updated

---
+++ _steps.js
@@ -0,0 +1,17 @@
+/**
+ * 1.create a folder (manually or npm mkdir dia project kholbo)
+ * 2.open command line that folder
+ * 3.npm  init
+ * 4.copy and paste npm install express
+ * 5.create index.js file
+ * ----------------------------
+ * for Express/node server
+ * ----------------------------
+ *1. require express
+ * 2. create app variable
+ * 3. declare port(process.env.PORT || 5000;) evabe dibo
+ * 4. set app.get('/',2 parameter er function likhbo)
+ * 5. listen to port
+ *6. node index.js ei command dibo command line.
+ * 7. check my browser for that port
+ */
